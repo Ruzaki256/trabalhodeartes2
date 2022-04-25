dc22338e6ee64a64b0aaeb5a4dda581a2e94c361
Ruzaki256 <104333355+Ruzaki256@users.noreply.github.com>
2022-04-25 17:37:14 -0300

    commit init
    primeira mudança

---
+++ Trabalho de artes/Trabalho.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# route -> hashtagtreinamentos.com/
+# função -> o que você quer exibir naquela página
+# template
+
+@app.route("/")
+def homepage():
+    return render_template("arthur.html")
+
+
+
+# colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    # servidor do heroku
